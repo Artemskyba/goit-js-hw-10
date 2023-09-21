c47a22daf3c974f02bc69064fbba28092f73a2f7
Artemskyba <artemgennadievich@icloud.com>
2023-09-21 15:20:38 +0300

    first_commit

---
+++ src/cat-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_KtW5wiUT0oWdCvrl2T4ASvtjwmmaA84BRAFCXJImumxTTTsDsQYclKdAfvQGFJGE';
+
+export function fechCatsBreeds() {
+  return axios.get('https://api.thecatapi.com/v1/breeds').then(response => {
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error(response.status);
+    }
+    return response.data;
+  });
+}
+
+export function fetchCatInfo(catId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${catId}`)
+    .then(response => {
+      if (response.status < 200 || response.status >= 300) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_KtW5wiUT0oWdCvrl2T4ASvtjwmmaA84BRAFCXJImumxTTTsDsQYclKdAfvQGFJGE';
+
+import { fechCatsBreeds } from './cat-api';
+import { fetchCatInfo } from './cat-api';
+
+const breedSelectEl = document.querySelector('.breed-select');
+const catInfoEl = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
+
+breedSelectEl.addEventListener('change', onSelectField);
+
+fechCatsBreeds()
+  .then(breeds => renderBreedSelect(breeds))
+  .catch(error => {
+    errorMessage(error);
+  });
+
+function renderBreedSelect(breeds) {
+  const breedSelectMarkup = breeds
+    .map(({ id, name }) => {
+      return `<option value="${id}">${name}</option>`;
+    })
+    .join('');
+  breedSelectEl.innerHTML = breedSelectMarkup;
+  breedSelectEl.classList.remove('visually-hidden');
+  loaderEl.classList.add('visually-hidden');
+}
+
+function onSelectField() {
+  loaderEl.classList.remove('visually-hidden');
+  catInfoEl.classList.add('visually-hidden');
+  fetchCatInfo(this.value)
+    .then(catInfo => renderCatCardMarkup(catInfo))
+    .catch(error => {
+      errorMessage();
+    });
+}
+
+function renderCatCardMarkup(catInfo) {
+  const catCardMarkup = catInfo
+    .map(({ url, breeds }) => {
+      const breedInfoMarkup = breeds
+        .map(({ description, id, name, temperament }) => {
+          return `<img class="cat-img" src="${url}" alt="${id}"/>
+            <h2 class="cat-breed">${name}</h2>
+            <p class="cat-description">${description}</p>
+            <p class="cat-temperament"><b>Temperament: </b>${temperament}</p>`;
+        })
+        .join('');
+      return breedInfoMarkup;
+    })
+    .join('');
+  catInfoEl.innerHTML = catCardMarkup;
+  loaderEl.classList.add('visually-hidden');
+  catInfoEl.classList.remove('visually-hidden');
+}
+
+function errorMessage() {
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
+}
+
+//error options
+//Замість select.breed-select можеш використовувати будь-яку бібліотеку з красивими селектом, наприклад https://slimselectjs.com/
+//Замість p.loader можеш використовувати будь-яку бібліотеку з красивими CSS-завантажувачами, наприклад https://cssloaders.github.io/
+//catCard styles
